69ad78d1a3f8b9d2df31e3faeba912c18cdf7a96
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2023-05-01 00:35:30 +0200

    feat: logout

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,8 @@
-const homePage = document.querySelector('.home-page');
-
 if (window.localStorage.getItem('retroGamesUser') === null) {
     window.location.href = '/login';
-} else {
-    homePage.classList.remove('d-none');
-    homePage.classList.add('d-flex');
 }
+
+document.getElementById('btn-logout').addEventListener('click', () => {
+    window.localStorage.removeItem('retroGamesUser');
+    window.location.href = '/login';
+});
