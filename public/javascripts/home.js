f0c601f4562de3e66a3f06ce20a990a4db265f03
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2023-05-01 23:38:19 +0200

    feat: basic home elements

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,15 @@
-if (window.localStorage.getItem('retroGamesUser') === null) {
+const homeWelcomeMessage = document.querySelector('.welcome-message');
+const homeAvatarImg = document.querySelector('.avatar-home-img');
+
+const user = window.localStorage.getItem('retroGamesUser');
+
+if (user === null) {
     window.location.href = '/login';
+} else {
+    const userInfo = JSON.parse(user);
+
+    homeWelcomeMessage.innerHTML = 'Bienvenido ' + userInfo.username;
+    homeAvatarImg.setAttribute('src', '/images/' + userInfo.avatar + '.jpg');
 }
 
 document.getElementById('btn-logout').addEventListener('click', () => {
