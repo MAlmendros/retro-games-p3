08803ff3de8de6c4832a5f22dab753c499f46a50
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2023-05-02 19:22:29 +0200

    feat: limit games to 2 players
    (cherry picked from commit 3ba820c16e0d26707290887e9c9012882b1f2059)

--- data/game.data.js
+++ data/game.data.js
@@ -8,64 +8,20 @@ let games = [
     {
         id: 2,
         name: 'Sala de Juego 2',
-        limit: 4,
-        players: [
-            {
-                id: 1,
-                username: 'Marian Almendros',
-                email: 'malmendros@uoc.edu',
-                password: '123456',
-                avatar: 'avatar-3'
-            },
-            {
-                id: 2,
-                username: 'José Antonio Cardenal',
-                email: 'jcardenal@uoc.edu',
-                password: '123456',
-                avatar: 'avatar-2'
-            }
-        ]
+        limit: 2,
+        players: []
     },
     {
         id: 3,
         name: 'Sala de Juego 3',
-        limit: 4,
+        limit: 2,
         players: []
     },
     {
         id: 4,
         name: 'Sala de Juego 4',
-        limit: 4,
-        players: [
-            {
-                id: 1,
-                username: 'Marian Almendros',
-                email: 'malmendros@uoc.edu',
-                password: '123456',
-                avatar: 'avatar-3'
-            },
-            {
-                id: 2,
-                username: 'José Antonio Cardenal',
-                email: 'jcardenal@uoc.edu',
-                password: '123456',
-                avatar: 'avatar-2'
-            },
-            {
-                id: 3,
-                username: 'Jacobo Bajo',
-                email: 'jbajog@uoc.edu',
-                password: '123456',
-                avatar: 'avatar-1'
-            },
-            {
-                id: 4,
-                username: 'Gaspar Delgado',
-                email: 'gdelgador@uoc.edu',
-                password: '123456',
-                avatar: 'avatar-2'
-            }
-        ]
+        limit: 2,
+        players: []
     }
 ];
 
